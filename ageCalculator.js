5f5f7943e9d03497d093870924a55328ccf4fb36
brandonsamuel1 <brandonsamuel72@gmail.com>
2017-10-11 05:09:39 +0000

    Add conditionals exercise

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old"
+}
+
+console.log(ageCalculator("Miranda", 1983, 2017));
+console.log(ageCalculator("Ferdinand", 1988, 2017));
---
+++ go-outside.js
@@ -0,0 +1,13 @@
+
+var temperature = 20;
+
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleevs won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
